fd47702ec8bd8a00711b8a3d21295c9ed101918b
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-09-28 20:59:04 +0900

    Learn generic2

--- typescript/src/generic/generic2.ts
+++ typescript/src/generic/generic2.ts
@@ -1,12 +1,23 @@
-class LocalDB {
+interface DB<T> {
+    add(v: T): void;
+    get(): T;
+}
+
+class LocalDB<T> {
     constructor(private localStroageKey: string) {
 
     }
-    add(v) {
+    add(v: T) {
         localStorage.setItem(this.localStroageKey, JSON.stringify(v));
     }
-    get() {
+    get(): T {
         const v = localStorage.getItem(this.localStroageKey);
         return (v) ? JSON.parse(v) : null;
     }
 }
+
+interface User { name: string }
+
+const userDb = new LocalDB<User>('user');
+userDb.add({ name: 'jay' });
+const user1A= userDb.get();
