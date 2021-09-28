0f36db214708a1f4ed303f41cb8b43320096198a
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2021-09-28 21:33:03 +0900

    Learn index-type

---
+++ typescript/src/type/index.ts
@@ -0,0 +1,29 @@
+interface Props {
+    name: string;
+    [key: string]: string;
+}
+
+const p: Props = {
+    name: 'hello',
+    a: 'd',
+    b: 'e',
+    c: '3',
+    0: 'd',
+    1: 'f'
+}
+
+let keys: keyof Props;
+
+interface User {
+    name: string;
+    age: number;
+    hello(msg: string): void;
+}
+
+let keysOfUser: keyof User;
+keysOfUser = "age"
+
+let helloMethod: User["hello"];
+helloMethod = function(msg: string) {
+
+}
